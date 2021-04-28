81a78057f7e8758251836440b30a663e11c119cc
Pete-Angjelevski <73816283+Pete-Angjelevski@users.noreply.github.com>
2021-04-28 21:17:33 +1200

    Initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    path: path.join(__dirname, '../server/public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../server/public')
+  }
+}
